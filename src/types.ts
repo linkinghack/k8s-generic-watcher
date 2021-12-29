da17d42e429a82a12f4f063a2cca70d817ccd088
linkinghack <linkinghack@outlook.com>
2021-12-29 23:19:22 +0800

    Add k8s API watcher class definition

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export const GROUP_CORE = "core";
+
+// Configures how to authenticate with the API server.
+//    BearerToken for in-cluster authentication, ClientCertificate for out-of-cluster authentication.
+export const AUTH_TYPE_TOKEN = "BearerToken";
+export const AUTH_TYPE_CERT = "ClientCertificate";
