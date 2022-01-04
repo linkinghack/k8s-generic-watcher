9ca509d55248ab9a5fb4ed03a053fb99672ee461
linkinghack <linkinghack@outlook.com>
2022-01-04 16:42:53 +0800

    add unit test for K8sClient

--- src/tests/K8sClientTest.ts
+++ src/tests/K8sClientTest.ts
@@ -7,25 +7,33 @@ export function testK8sClientCreation() {
         autoInclusterConfig: false,
         authType: "ClientCertificate",
         tokenFilePath: "",
-        clientCertPath: "/Users/liulei/.minikube/profiles/minikube/client.crt",
-        clientKeyPath: "/Users/liulei/.minikube/profiles/minikube/client.key",
-        caCertPath: "/Users/liulei/.minikube/ca.crt"
+        clientCertPath: "/Users/liulei/.kube/minikube/profiles/minikube/client.crt",
+        clientKeyPath: "/Users/liulei/.kube/minikube/profiles/minikube/client.key",
+        caCertPath: "/Users/liulei/.kube/minikube/ca.crt",
+        autoKeepAlive: false,
+        autoReconnect: false
     }
-    const client = new K8sClient("https://172.16.67.11:8443", options);
+    const client = new K8sClient("https://k8s.office.linkinghack.com:8443", options);
     let stream = client.request("/api/v1/namespaces/default/pods", {});
-    console.log("Request: ", stream);
     
     stream.on("response", (headers: http2.IncomingHttpHeaders, flags: number) => {
         console.log(headers);
     })
     stream.on("data", (chunk: Buffer) => {
-        console.log(chunk.toString());
+        console.log("data received");
     });
     
     stream.on("end", () => {
         console.log("HTTP2 stream end");
     });
 
+    stream.on("close", () => {
+        console.log("HTTP2 stream close");
+        client.close();
+    })
+
 }
 
-testK8sClientCreation();
+export default {
+    testK8sClientCreation
+}
---
+++ src/tests/UnitTests.ts
@@ -0,0 +1,3 @@
+import k8sClientTests from './K8sClientTest';
+
+k8sClientTests.testK8sClientCreation();
